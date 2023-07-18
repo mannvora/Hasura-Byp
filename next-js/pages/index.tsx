4be9aadcc9e69c0e861dd07470b9d696af53b9cc
mann <mannvora19683@gmail.com>
2023-07-18 18:01:58 +0530

    deployed friends

--- next-js/pages/index.tsx
+++ next-js/pages/index.tsx
@@ -1,16 +1,66 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { GetStaticPaths, GetStaticProps, GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { Key } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type Data = {
+  friends: Record<string, string>[]
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+  let friends;
+
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string, {
+      method: 'POST',
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET as string,
+      },
+      body: JSON.stringify({
+        query: 
+        `query {
+          friend {
+            name
+          }
+        }`
+      })
+    });
+
+    const result = await response.json();
+    const data = result.data;
+
+    friends = data.friend;
+
+    console.log(friends); 
+  } catch(error){
+    console.log(error);
+  }
+
+  return { 
+    props: 
+    {
+      friends
+    } 
+  }
+}
+
+export default function Home({friends}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="flex flex-col items-center justify-center w-full lg:flex-row">
-          <span>HELLO WORLD</span>
+          {
+            friends.map((friend: {name: string}) => {
+              return (
+                <span key = {friend.name} className='text-blue-400 p-6'>{friend?.name}</span>
+              )
+            })
+          }
         </div>
       </div>
     </main>
